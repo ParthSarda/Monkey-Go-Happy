bd56fec875dc8ee46471b23400d489f69ed0c90e
ParthSarda <72331871+ParthSarda@users.noreply.github.com>
2020-10-18 11:35:31 +0530

    Add files via upload

---
+++ Template_copy_2020_10_18_06_03_09/sketch.js
@@ -0,0 +1,101 @@
+
+var monkey , monkey_running;
+var banana ,bananaImage, obstacle, obstacleImage;
+var FoodGroup, obstacleGroup;
+var survivalTime=0; 
+var score=0;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  createCanvas(400,400);
+  
+  monkey=createSprite(80,315,30,30);
+  monkey.addAnimation("running",monkey_running); 
+  monkey.scale=0.125;
+  
+  ground=createSprite(300,340,600,15);
+  ground.velocityX=-3;
+  
+  FoodGroup=new Group();
+  ObstacleGroup=new Group();
+  
+}
+
+
+function draw() {
+  background("white");
+  
+  stroke=("white");
+  textSize=(20);
+  fill("white");
+  text("Score:"+score,300,300);
+  
+    stroke=("black");
+  textSize=(30);
+  fill("black");
+  survivalTime=Math.ceil(frameCount/frameRate())
+  text("Survival time:"+survivalTime,100,50)
+  
+    if (ground.x < 150){
+      ground.x = ground.width/2;
+    }
+  
+   if(keyDown("space")&& monkey.y >= 250) {
+        monkey.velocityY = -13;
+    }
+  
+   monkey.velocityY = monkey.velocityY + 0.8;
+  monkey.collide(ground);
+  
+  food();
+  obstacles();
+  
+  
+
+
+  drawSprites();
+}
+
+function food(){
+  
+  if(frameCount%80===0){
+    
+    banana=createSprite(400,Math.round(random(120,200)),20,20);
+    banana.addImage(bananaImage);
+    banana.scale=0.1;
+    banana.velocityX=-3;
+    banana.lifetime=134;
+    
+    FoodGroup.add(banana);
+}
+  
+  
+}
+
+function obstacles(){
+  
+  if(frameCount%300===0){
+     obstacle=createSprite(400,285,20,20);
+    obstacle.addImage(obstacleImage);
+    obstacle.lifetime=134;
+    obstacle.scale=0.2;
+    obstacle.velocityX=-6;
+   }
+}
+
+
+
+
+
+
